0618d77531db94dbf9ae4cfd290b2ee7424442a0
ravindu <ravindulakshitha250@gmail.com>
2024-10-17 14:16:40 +0530

    change background color and create navbar

--- lecturehallallocation/src/App.js
+++ lecturehallallocation/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NavbarCom from "./Components/NavBar/navBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundColor: "rgb(59.61% 16.86% 10.98%)", height: "100vh", color: "white" }}>
+      <NavbarCom />
     </div>
   );
 }
